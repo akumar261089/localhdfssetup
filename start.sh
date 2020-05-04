8633b0e611217805be3c02bb3b510827dbdca636
Abhinav <akumar261089@gmail.com>
2020-05-04 18:41:21 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-SERVER_IP=31.171.244.132
-SERVER_USER=cloudsigma
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    echo "run sh start.sh <VM_IP> <VM_USERNAME>"
+fi
+SERVER_IP=$1
+SERVER_USER=$2
 
 
 scp -r execute $SERVER_USER@$SERVER_IP:/tmp/

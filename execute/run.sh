e6ef2c4e662a757b7ae01af6e8a0cbe1253f192a
Abhinav <akumar261089@gmail.com>
2020-05-05 19:13:43 +0530

    Update run.sh

--- execute/run.sh
+++ execute/run.sh
@@ -6,12 +6,13 @@ apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install
        curl\
       netcat\
        gnupg
-
+       
+export HADOOP_VERSION=3.2.1
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64/
 export HADOOP_HOME=/opt/hadoop-$HADOOP_VERSION
 export HADOOP_CONF_DIR=/etc/hadoop
 export PATH=$HADOOP_HOME/bin/:$PATH
-export HADOOP_VERSION=3.2.1
+
 curl -fSL  https://www.apache.org/dist/hadoop/common/hadoop-$HADOOP_VERSION/hadoop-$HADOOP_VERSION.tar.gz -o /tmp/hadoop.tar.gz
 tar -xf /tmp/hadoop.tar.gz -C /opt/
 rm /tmp/hadoop.tar.gz*
